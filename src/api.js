a7e3a84201a6a611935a34922059715908debf44
marcelobs87 <marceloschenkel@gmail.com>
2022-06-09 20:10:39 -0300

    const getCountry no api.js

--- src/api.js
+++ src/api.js
@@ -5,12 +5,13 @@ const headers = {
     mode: 'cors',
     cache: 'default'
 }
-const getCountry = 
 
 function getCountry(country) {
     return fetch(`${path}/${country}`, headers)
     .then((response) => response.json())
 }
+
+const getCountry = getCountry
 
 export default {
     getCountry
